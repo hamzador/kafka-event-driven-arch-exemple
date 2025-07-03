b3beabf77d543c39f5ba8f7fb7086e391953c149
hamzador <hamozadouyry@gmail.com>
2025-07-03 17:14:42 +0200

    grouper les resultat en comptant le nombre de visite par persone

--- src/main/java/hdouyry/kafka/demo/handlers/PageEventHandler.java
+++ src/main/java/hdouyry/kafka/demo/handlers/PageEventHandler.java
@@ -2,12 +2,19 @@ package hdouyry.kafka.demo.handlers;
 
 
 import hdouyry.kafka.demo.event.PageEvent;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -35,5 +42,15 @@ public class PageEventHandler {
                     10 + new  Random().nextInt(10000)
             );
         };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
+        return (input) ->
+                input.filter((k,v) -> v.getDuration()>100)
+                        .map((k,v)-> new KeyValue<>(v.getName(), v.getDuration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .count()
+                        .toStream();
     }
 }
